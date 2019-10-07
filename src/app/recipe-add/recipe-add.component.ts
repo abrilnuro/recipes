f515bb80bd5711ead2bb7748e6d200454040ba37
abril <13170789@itculiacan.edu.mx>
2019-10-06 22:50:11 -0600

    se agrega ingredientes en add recipe

--- src/app/recipe-add/recipe-add.component.ts
+++ src/app/recipe-add/recipe-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Recipe } from '../shared/recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-add',
@@ -6,10 +8,13 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./recipe-add.component.css']
 })
 export class RecipeAddComponent implements OnInit {
-  @ViewChild('newIngredientName', {static: false}) newIngredientName: ElementRef;
-  @ViewChild('newIngredientImage', {static: false}) newIngredientImage: ElementRef;
-  @ViewChild('newIngredientStep', {static: false}) newStep: ElementRef;
+  @ViewChild('newRecipeName', {static: false}) newRecipeName: ElementRef;
+  @ViewChild('newRecipeImage', {static: false}) newRecipeImage: ElementRef;
+  @ViewChild('newRecipetStep', {static: false}) newRecipetStep: ElementRef;
+  @ViewChild('newRecipetIngredient', {static: false}) newRecipetIngredient: ElementRef;
+  @ViewChild('newRecipetIngredientAmount', {static: false}) newRecipetIngredientAmount: ElementRef;
   stepsList: string[] = [];
+  ingredientsList: Ingredient[] = [];
   nStep: number = 1;
 
   constructor() { }
@@ -18,13 +23,33 @@ export class RecipeAddComponent implements OnInit {
   }
 
   addStep() {
-    this.stepsList.push(this.newStep.nativeElement.value);
+    this.stepsList.push(this.newRecipetStep.nativeElement.value);
     this.nStep = this.stepsList.length + 1;
-    this.newStep.nativeElement.value = '';
+    this.newRecipetStep.nativeElement.value = '';
   }
 
   deleteStep(stepIndex: number) {
     this.stepsList.splice(stepIndex, 1);
     this.nStep = this.stepsList.length + 1;
+  }
+
+  addIngredient() {
+    const name = this.newRecipetIngredient.nativeElement.value;
+    const amount = this.newRecipetIngredientAmount.nativeElement.value;
+    const ingredient = new Ingredient(name, amount, '', '');
+    this.ingredientsList.push(ingredient);
+
+    this.newRecipetIngredient.nativeElement.value = '';
+    this.newRecipetIngredientAmount.nativeElement.value = '';
+  }
+
+  deleteIngredient(ingredientIndex: number) {
+    this.ingredientsList.splice(ingredientIndex, 1);
+  }
+
+  addRecipe() {
+    const name = this.newRecipeName.nativeElement.value;
+    const image = '';
+    //let recipe = new Recipe();
   }
 }
